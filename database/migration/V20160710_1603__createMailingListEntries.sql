d9fdef310c606b7dfc68cd7284263509aba3700a
Nick Magerko <nmagerko@gmail.com>
2016-07-10 16:10:23 -0500

    rename mailing list entries table

--- database/migration/V20160710_1603__createMailingListEntries.sql
+++ database/migration/V20160710_1603__createMailingListEntries.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `mailing_lists` (
+CREATE TABLE `mailing_list_entries` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` INT UNSIGNED NOT NULL,
   `list` ENUM('idlers', 'applicants', 'accepted', 'waitlisted', 'software', 'hardware', 'open_source', 'admins', 'staff', 'sponsors', 'mentors', 'volunteers') NOT NULL,
--- database/revert/V20160710_1603__createMailingLists.revert.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE `mailing_lists`;
