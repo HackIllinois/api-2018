6f6d81defe5e00e24941fa3045710d2ef54870db
tommypacker <tommypacker@gmail.com>
2016-10-07 22:30:58 -0500

    Added string fetching

--- api/v1/controllers/CachingController.js
+++ api/v1/controllers/CachingController.js
@@ -7,17 +7,55 @@ var config = require('../../config');
 var middleware = require('../middleware');
 var router = require('express').Router();
 
+
 function saveString (req, res, next) {
 	var key = req.body.key;
-	var params = req.body.value;
+	var value = req.body.value;
 
-	services.RedisCacheService.storeString(key, params)
+	services.RedisCacheService.storeString(key, value)
 		.then(function (status){
 			res.body = {};
 			res.body.status = status;
 			next();
 			return null;
-		});	
+		})
+		.catch(function(error){
+			next(error);
+			return null;
+		});
+}
+
+function saveHash (req, res, next) {
+	var key = req.body.key;
+	var keyVals = req.body.keyVals;
+
+	services.RedisCacheService.storeHash(key, keyVals)
+		.then(function (status){
+			res.body = {};
+			res.body.status = status;
+			next();
+			return null;
+		})
+		.catch(function(error){
+			next(error);
+			return null;
+		});
+}
+
+function getString (req, res, next) {
+	var key = req.query.key;
+
+	services.RedisCacheService.getString(key)
+		.then(function (string){
+			res.body = {}
+			res.body.value = string;
+			next();
+			return null;
+		})
+		.catch(function(error){
+			next(error);
+			return null;
+		});
 }
 
 
@@ -26,6 +64,8 @@ router.use(middleware.auth);
 router.use(middleware.request);
 
 router.post('/saveString', saveString);
+router.post('/saveHash', saveHash);
+router.get('/getString', getString);
 
 router.use(middleware.response);
 router.use(middleware.errors);
--- api/v1/services/RedisCacheService.js
+++ api/v1/services/RedisCacheService.js
@@ -14,7 +14,7 @@ function _storeHash (key, keyValuePairs) {
 	});
 }
 
-function _storeString(key, value) {
+function _storeString (key, value) {
 	return client.setAsync(key, value).then(function(reply){
 		return reply;
 	});
@@ -24,10 +24,32 @@ function _storeList() {
 	return null;
 }
 
+function _getString (key) {
+	return client.getAsync(key).then(function(res){
+		return res;
+	});
+}
+
+function _stringKeyExists (key) {
+	return client.existsAsync(key)
+		.then(function(reply){
+			if(reply != 1){
+				//TODO: Create new error for non-existent keys
+				throw new errors.ApiError();
+			}else{
+				return _getString(key);
+			}
+		});
+}
+
 module.exports.storeHash = function (key, keyValuePairs) {
 	return _Promise.resolve(_storeHash(key, keyValuePairs));
 }
 
 module.exports.storeString = function (key, value) {
 	return _Promise.resolve(_storeString(key, value));
+}
+
+module.exports.getString = function (key) {
+	return _Promise.resolve(_stringKeyExists(key));
 }
