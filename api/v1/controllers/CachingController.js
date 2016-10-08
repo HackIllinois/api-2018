6c175b5d05135f7dd5115da66203947466852e32
tommypacker <tommypacker@gmail.com>
2016-10-07 22:50:04 -0500

    Add create/read operations for redis strings

--- api/v1/controllers/CachingController.js
+++ api/v1/controllers/CachingController.js
@@ -42,6 +42,23 @@ function saveHash (req, res, next) {
 		});
 }
 
+function saveList (req, res, next) {
+	var key = req.body.key;
+	var vals = req.body.vals;
+
+	services.RedisCacheService.storeList(key, vals)
+		.then(function (status){
+			res.body = {};
+			res.body.status = status;
+			next();
+			return null;
+		})
+		.catch(function(error){
+			next(error);
+			return null;
+		});
+}
+
 function getString (req, res, next) {
 	var key = req.query.key;
 
@@ -58,6 +75,24 @@ function getString (req, res, next) {
 		});
 }
 
+function getList (req, res, next) {
+	var key = req.query.key;
+	var start = req.query.start;
+	var end = req.query.end;
+
+	services.RedisCacheService.getList(key, start, end)
+		.then(function (list){
+			res.body = {}
+			res.body.values = list;
+			next();
+			return null;
+		})
+		.catch(function(error){
+			next(error);
+			return null;
+		});
+}
+
 
 router.use(bodyParser.json());
 router.use(middleware.auth);
@@ -65,7 +100,9 @@ router.use(middleware.request);
 
 router.post('/saveString', saveString);
 router.post('/saveHash', saveHash);
+router.post('/saveList', saveList);
 router.get('/getString', getString);
+router.get('/getList', getList);
 
 router.use(middleware.response);
 router.use(middleware.errors);
--- api/v1/services/RedisCacheService.js
+++ api/v1/services/RedisCacheService.js
@@ -20,8 +20,10 @@ function _storeString (key, value) {
 	});
 }
 
-function _storeList() {
-	return null;
+function _storeList (key, values) {
+	return client.rpushAsync(key, values).then(function(reply){
+		return reply;
+	});
 }
 
 function _getString (key) {
@@ -29,6 +31,12 @@ function _getString (key) {
 		return res;
 	});
 }
+
+function _getList (key, startIndex, endIndex) {
+	return client.lrangeAsync(key, startIndex, endIndex).then(function(res){
+		return res;
+	});
+} 
 
 function _stringKeyExists (key) {
 	return client.existsAsync(key)
@@ -42,6 +50,18 @@ function _stringKeyExists (key) {
 		});
 }
 
+function _listKeyExists (key, startIndex, endIndex) {
+	return client.existsAsync(key)
+		.then(function(reply){
+			if(reply != 1){
+				//TODO: Create new error for non-existent keys
+				throw new errors.ApiError();
+			}else{
+				return _getList(key, startIndex, endIndex)
+			}
+		});
+}
+
 module.exports.storeHash = function (key, keyValuePairs) {
 	return _Promise.resolve(_storeHash(key, keyValuePairs));
 }
@@ -50,6 +70,14 @@ module.exports.storeString = function (key, value) {
 	return _Promise.resolve(_storeString(key, value));
 }
 
+module.exports.storeList = function (key, values) {
+	return _Promise.resolve(_storeList(key, values));
+}
+
 module.exports.getString = function (key) {
 	return _Promise.resolve(_stringKeyExists(key));
+}
+
+module.exports.getList = function (key, startIndex, endIndex) {
+	return _Promise.resolve(_listKeyExists(key, startIndex, endIndex));
 }
