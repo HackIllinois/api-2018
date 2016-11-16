2f5db0b0a6bfe8d98f7c2b5adbedb59ecd2a70e0
Yash Sharma <yashosharma@gmail.com>
2016-11-16 02:28:02 -0600

    some refactoring

--- api/v1/controllers/HealthController.js
+++ api/v1/controllers/HealthController.js
@@ -3,13 +3,16 @@ var middleware = require('../middleware');
 
 var router = require('express').Router();
 
+function healthCheck(req, res, next) {
+    next();
+    return null;
+}
+
 router.use(bodyParser.json());
+router.use(middleware.auth);
 router.use(middleware.request);
 
-router.get('/', function(req, res) {
-    res.send();
-});
-
+router.get('', healthCheck);
 router.use(middleware.response);
 router.use(middleware.errors);
 
