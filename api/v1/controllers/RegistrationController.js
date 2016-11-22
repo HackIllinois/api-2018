7f4a45510c9a772023af87c1bbe375f90ee7a22c
Nick Magerko <nmagerko@gmail.com>
2016-11-22 14:52:37 -0600

    converted request middleware into stand-alone middleware

--- api/v1/controllers/RegistrationController.js
+++ api/v1/controllers/RegistrationController.js
@@ -2,6 +2,7 @@ var bodyParser = require('body-parser');
 
 var services = require('../services');
 var middleware = require('../middleware');
+var requests = require('../requests');
 var roles = require('../utils/roles');
 
 var router = require('express').Router();
@@ -103,11 +104,14 @@ router.use(bodyParser.json());
 router.use(middleware.auth);
 router.use(middleware.request);
 
-router.post('/mentor', middleware.permission(roles.NONE, _isAuthenticated), createMentor);
+router.post('/mentor', middleware.request(requests.MentorRequest),
+	middleware.permission(roles.NONE, _isAuthenticated), createMentor);
 router.get('/mentor', middleware.permission(roles.MENTOR), fetchMentorByUser);
 router.get('/mentor/:id', middleware.permission(roles.ORGANIZERS), fetchMentorById);
-router.put('/mentor', middleware.permission(roles.MENTOR), updateMentorByUser);
-router.put('/mentor/:id', middleware.permission(roles.ORGANIZERS), updateMentorById);
+router.put('/mentor', middleware.request(requests.MentorRequest),
+	middleware.permission(roles.MENTOR), updateMentorByUser);
+router.put('/mentor/:id', middleware.request(requests.MentorRequest),
+	middleware.permission(roles.ORGANIZERS), updateMentorById);
 
 router.use(middleware.response);
 router.use(middleware.errors);
