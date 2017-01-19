aa0f6c567302d5deaf5b5aa1bae32cc8d9231dfa
Nick Magerko <nmagerko@gmail.com>
2017-01-18 19:33:47 -0600

    simplified logging and added debug log statements

--- api/logging.js
+++ api/logging.js
@@ -1,34 +1,17 @@
 var winston = require("winston");
-var WinstonCloudwatch = require("winston-cloudwatch");
 
 var config = require('./config');
-var files = require('./files');
 
 var transports = [];
-if (config.isProduction && config.aws.enabled) {
-	var cloudwatchTransport = new WinstonCloudwatch({
-		logGroupName: config.logs.groupName,
-		logStreamName: config.logs.streamPrefix + '/' + config.logs.groupName + '/' + config.id,
-		awsRegion: config.aws.defaults.region,
-		json: true
-	});
+var consoleTransport = new winston.transports.Console({
+	colorize: !config.isProduction,
+	prettyPrint: !config.isProduction,
+	stringify: config.isProduction,
+	json: config.isProduction
+});
 
-	// add other transports as needed
-	transports.push(cloudwatchTransport);
-} else {
-	var fileTransport = new winston.transports.File({
-		filename: files.initializeLogfile()
-	});
-	var consoleTransport = new winston.transports.Console({
-		colorize: true,
-		prettyPrint: true
-	});
-
-	// add other transports as needed
-	transports.push(fileTransport);
-	transports.push(consoleTransport);
-}
-
+// add other transports as needed
+transports.push(consoleTransport);
 
 var logger = new winston.Logger({ transports: transports });
 logger.info("prepared environment for %s", config.environment);
