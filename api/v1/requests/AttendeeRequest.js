9e0d1cc3994993a6bfa77b108ec7e4a1cdb28eae
tommypacker <tommypacker@gmail.com>
2017-01-26 21:10:24 -0600

    Fix conflict

--- api/v1/requests/AttendeeRequest.js
+++ api/v1/requests/AttendeeRequest.js
@@ -49,7 +49,6 @@ var bodyValidations = {
 	'attendee.isPrivate': ['required', 'boolean'],
 	'attendee.hasLightningInterest': ['boolean'],
 	'attendee.phoneNumber': ['string', 'maxLength:15'],
-	'attendee.ecosystemSelected': ['integer'],
 	'ecosystemInterests': ['required', 'array', 'minLength:1', 'maxLength:4', validators.array(validators.nested(ecosystemInterestValidations, 'ecosystemInterests'), 'ecosystemInterests')],
 	'projects': ['array', 'maxLength:2', registration.verifyProjectArray, validators.array(validators.nested(projectValidations, 'projects'), 'projects')],
 	'extras': ['array', 'maxLength:3', validators.array(validators.nested(extraInfoValidations, 'extras'), 'extras')],
