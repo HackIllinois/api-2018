44375dbf44284ab56c7a7c9b2ce23e7a87eaa86e
Yash Sharma <yashosharma@gmail.com>
2017-02-09 20:10:08 -0600

    Added event tracking migrations

--- database/revert/V20170114_2116__createAttendeeRSVP.revert.sql
+++ database/revert/V20170114_2116__createAttendeeRSVP.revert.sql
@@ -1,1 +1,5 @@
 DROP TABLE `attendee_rsvps`;
+DELETE FROM `mailing_lists` WHERE `name`='attendees';
+INSERT INTO `mailing_lists` (`name`) VALUES ('software');
+INSERT INTO `mailing_lists` (`name`) VALUES ('hardware');
+INSERT INTO `mailing_lists` (`name`) VALUES ('open_source');
