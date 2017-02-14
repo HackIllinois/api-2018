137b047643cbbb896520c7f8b0107c19df22cab6
Yash Sharma <yashosharma@gmail.com>
2017-02-14 00:57:50 -0600

    Yash addresses Yashas' comments

--- api/v1/services/TrackingService.js
+++ api/v1/services/TrackingService.js
@@ -1,6 +1,7 @@
 var CheckitError = require('checkit').Error;
 var _ = require('lodash');
 var _Promise = require('bluebird');
+var ms = require('ms');
 
 var cache = require('../../cache').instance();
 var TrackingItem = require('../models/UniversalTrackingItem');
@@ -31,7 +32,7 @@ module.exports.createTrackingEvent = function (attributes) {
             if(!_.isNil(result)) {
                 return cache.ttlAsync('trackedEvent')
                     .then(function (ttl) {
-                        var message = "An event is already being tracked at this time, seconds left: " + ttl;
+                        var message = "An event is already being tracked at this time, time left: " + ttl.toHMS();
                         var source = trackingItem.get('name');
                         return _Promise.reject(new errors.InvalidTrackingStateError(message, source));
                     });
@@ -59,7 +60,7 @@ module.exports.addEventParticipant = function(participantId) {
             return cache.getAsync(participantId + TrackingFooter)
                 .then(function (result) {
                     if(!_.isNil(result)) {
-                        var message = 'This attendee has already participated in this event!';
+                        var message = 'This attendee has already participated in ' + currentEvent + '!';
                         var source = participantId;
                         return _Promise.reject(new errors.InvalidParameterError(message, source));
                     }
@@ -80,3 +81,11 @@ module.exports.addEventParticipant = function(participantId) {
                 });
         });
 };
+
+Number.prototype.toHMS = function() {
+    d = this;
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
+}
