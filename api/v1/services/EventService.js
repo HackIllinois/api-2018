1ea8ea1ba21703bd3e2750d74a3b56d6b0f3cba3
Yash Sharma <yashosharma@gmail.com>
2017-02-22 02:55:19 -0600

    Transactions

--- api/v1/services/EventService.js
+++ api/v1/services/EventService.js
@@ -1,5 +1,7 @@
 var _ = require('lodash');
+var _Promise = require('bluebird');
 
+var Bookshelf = require('../../database').instance();
 var errors = require('../errors');
 var Location = require('../models/Location');
 var Event = require('../models/Event');
@@ -34,26 +36,23 @@ module.exports.createEvent = function (params) {
     var event = params.event;
     var locations = params.eventLocations;
 
-    var event = Event.forge({event});
+    return Bookshelf.transaction(function (t) {
+        return Event.findByName(event.name)
+            .then(function (result) {
+                if (!_.isNull(result)) {
+                    var message = "An event with the given name already exists";
+                    var source = "name";
+                    throw new errors.InvalidParameterError(message, source);
+                }
 
-    return Event.findByName(event.name)
-        .then(function (result) {
-            if (!_.isNull(result)) {
-                var message = "An event with the given name already exists";
-                var source = "name";
-                throw new errors.InvalidParameterError(message, source);
-            }
-
-            return event.save();
-        })
-        .then(function (event) {
-            return _Promise.map(locations, function (location) {
-                location.eventId = event.get('id');
-
-                return EventLocation.forge(location).save();
+                var event = Event.forge({event});
+                return event.save(null, {transacting: t});
+            })
+            .tap(function (result) {
+                return _Promise.map(locations, function(location) {
+                   return new EventLocation(location).save({'event_id':result.id}, {transacting: t});
+                });
             });
-        })
-        .then(function () {
-            return Event.findByName(event.name);
-        });
+    });
+
 };
