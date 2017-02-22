3a6385ef9352b2f82764f6f6415e37de75d78e1b
Yash Sharma <yashosharma@gmail.com>
2017-02-22 00:50:49 -0600

    Added event api models and migrations

---
+++ api/v1/models/Event.js
@@ -0,0 +1,32 @@
+var _ = require('lodash');
+var events = require('../utils/events');
+
+var Model = require('./Model');
+var EventLocation = require('./EventLocation');
+var Event = Model.extend({
+    tableName: 'events',
+    idAttribute: 'id',
+    validations: {
+        name: ['required', 'string', 'maxLength:255'],
+        shortName: ['required', 'string', 'maxLength:25'],
+        description: ['required', 'string', 'maxLength:2047'],
+        tracking: ['required', 'boolean'],
+        startTime: ['required', 'date'],
+        endTime: ['required', 'date'],
+        tag: ['required', 'string', events.verifyTag]
+    },
+    locations: function () {
+        return this.hasMany(EventLocation);
+    }
+});
+
+/**
+ * Finds an attendee by its ID, joining in its related project ideas
+ * @param  {Number|String} id	the ID of the model with the appropriate type
+ * @return {Promise<Model>}		a Promise resolving to the resulting model or null
+ */
+Event.findById = function (id) {
+    return Event.where({ id: id }).fetch({withRelated: ['locations']});
+};
+
+module.exports = Event;
