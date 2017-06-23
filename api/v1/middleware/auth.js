a542b84142dfb54f8f548bd6a92b66f232c1978d
Yash Sharma <yashosharma@gmail.com>
2017-06-22 17:48:50 -0700

    Added admin impersonation

--- api/v1/middleware/auth.js
+++ api/v1/middleware/auth.js
@@ -1,9 +1,12 @@
 const AuthService = require('../services/AuthService');
+const PermissionService = require('../services/PermissionService');
 const User = require('../models/User');
 const config = require('../../config');
 const errors = require('../errors');
+const _ = require('lodash');
 
 const AUTH_HEADER = config.auth.header;
+const ADMIN_USER_OVERRIDE_HEADER = 'Admin-User-Override';
 
 module.exports = (req, res, next) => {
   const auth = req.header(AUTH_HEADER);
@@ -17,12 +20,27 @@ module.exports = (req, res, next) => {
       // specifies that request supplied a valid auth token
       // (but not necessarily that the associated user data has been retrieved)
       req.auth = true;
-
       return User.findById(decoded.sub);
     })
     .then((user) => {
-      req.user = user;
+      const adminUserOverride = req.get('Admin-User-Override');
 
+      if (!_.isUndefined(adminUserOverride) && PermissionService.isOrganizer(user)) {
+        return User.findById(adminUserOverride)
+          .then((user) => {
+            if (_.isNull(user) || PermissionService.isOrganizer(user)) {
+              const message = 'The provided userId was invalid (' + adminUserOverride + ')';
+              const source = ADMIN_USER_OVERRIDE_HEADER;
+
+              return next(new errors.InvalidHeaderError(message, source));
+            }
+
+            req.user = user;
+            return next();
+          });
+      }
+
+      req.user = user;
       return next();
     })
     .catch(errors.UnprocessableRequestError, (error) => {
