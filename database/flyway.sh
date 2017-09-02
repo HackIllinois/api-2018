754a36b3db5ef6e948cb20b08bd60c464f68648b
Nick Magerko <nmagerko@gmail.com>
2017-09-02 17:26:57 -0500

    updated vagrant and flyway to use new setup; started updating travis.yml

--- database/flyway.sh
+++ database/flyway.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-source scripts/env.sh $2
-
-cd "$(dirname ${BASH_SOURCE[0]})"
+set -e
 
 if [ "$#" -ne 2 ]; then
 	echo "Usage: $0 <flyway command> <target>"
 	echo "Type 'flyway' for a list of available commands"
-	echo "Target is any available environment (usually dev or prod)"
+	echo "Target is any available environment (usually 'production' or 'development')"
 	exit 2
 fi
 
-flyway -user=$DB_USERNAME -password=$DB_PASSWORD  -url=jdbc:mysql://$DB_HOSTNAME:$DB_PORT/$DB_NAME -locations=filesystem:"$PWD/migration" -baselineOnMigrate=true -sqlMigrationSuffix=.sql $1
+FLYWAY_USERNAME=$DB_USERNAME
+FLYWAY_PASSWORD=$DB_PASSWORD
+FLYWAY_HOSTNAME=$DB_HOSTNAME
+FLYWAY_PORT=$DB_PORT
+FLYWAY_NAME=$DB_NAME
+
+cd "$(dirname ${BASH_SOURCE[0]})"
+if [ -f "../config/$2.json" ]; then
+	configuration=`echo -e $(cat ../config/$2.json)`
+	FLYWAY_USERNAME=`python -c "import json; print(json.loads('$configuration')['database']['primary']['user'])"`
+	FLYWAY_PASSWORD=`python -c "import json; print(json.loads('$configuration')['database']['primary']['password'])"`
+	FLYWAY_HOSTNAME=`python -c "import json; print(json.loads('$configuration')['database']['primary']['host'])"`
+	FLYWAY_PORT=`python -c "import json; print(json.loads('$configuration')['database']['primary']['port'])"`
+	FLYWAY_NAME=`python -c "import json; print(json.loads('$configuration')['database']['primary']['name'])"`
+else
+	echo "no configuration found for target '$2'. defaulting to environment"
+fi
+
+flyway -user=$FLYWAY_USERNAME -password=$FLYWAY_PASSWORD -url=jdbc:mysql://$FLYWAY_HOSTNAME:$FLYWAY_PORT/$FLYWAY_NAME -locations=filesystem:"$PWD/migration" -baselineOnMigrate=true -sqlMigrationSuffix=.sql $1
