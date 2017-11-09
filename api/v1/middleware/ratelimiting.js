606d3b7672f3b0027e651a452a469541d770fae2
Arnav Sankaran <arnavsankaran@gmail.com>
2017-11-09 00:39:29 -0600

    Documented delay window limit setting

--- api/v1/middleware/ratelimiting.js
+++ api/v1/middleware/ratelimiting.js
@@ -12,8 +12,8 @@ const ratelimiter = new BruteLimiter(store, {
   freeRetries: config.limit.count,
   attachResetToRequest: false,
   refreshTimeoutOnRequest: false,
-  minWait: config.limit.window * 1000 + 1,
-  maxWait: config.limit.window * 1000 + 1,
+  minWait: config.limit.window * 1000 + 1, // Set delay equal to just over the lifetime, * 1000 to convert to milliseconds
+  maxWait: config.limit.window * 1000 + 1, // Set delay equal to just over the lifetime, * 1000 to convert to milliseconds
   lifetime: config.limit.window
 });
 
