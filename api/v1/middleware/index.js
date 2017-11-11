9627fbf38b4257b72094fbe98d220e2146723dd7
shreychowdhary <shrey.chowdhary1@gmail.com>
2017-11-11 13:28:05 -0600

    Updated ratelimting to use context

--- api/v1/middleware/index.js
+++ api/v1/middleware/index.js
@@ -1,9 +1,10 @@
+const ctx = require("../../../ctx");
 module.exports = {
-  auth: require('./auth.js'),
+  auth: require('./auth.js')(ctx),
   errors: require('./errors.js'),
   permission: require('./permission.js'),
   response: require('./response.js'),
   request: require('./request.js'),
   upload: require('./upload.js'),
-  ratelimiting: require('./ratelimiting.js')
+  ratelimiting: require('./ratelimiting.js')(ctx)
 };
--- api/v1/middleware/ratelimiting.js
+++ api/v1/middleware/ratelimiting.js
@@ -1,20 +1,23 @@
-const cache = require('../../cache').instance();
-const config = require('../../config');
 
 const BruteLimiter = require('express-brute');
-
 const RedisStore = require('express-brute-redis');
-const store = new RedisStore({
-  client: cache
-});
 
-const ratelimiter = new BruteLimiter(store, {
-  freeRetries: config.limit.count,
-  attachResetToRequest: false,
-  refreshTimeoutOnRequest: false,
-  minWait: config.limit.window * 1000 + 1, // Set delay equal to just over the lifetime, * 1000 to convert to milliseconds
-  maxWait: config.limit.window * 1000 + 1, // Set delay equal to just over the lifetime, * 1000 to convert to milliseconds
-  lifetime: config.limit.window
-});
+module.exports = (ctx) => {
+    let cache = ctx.cache().instance();
+    let config = ctx.config();
 
-module.exports = ratelimiter.prevent;
+    let store = new RedisStore({
+      client: cache
+    });
+
+    let ratelimiter = new BruteLimiter(store, {
+      freeRetries: config.limit.count,
+      attachResetToRequest: false,
+      refreshTimeoutOnRequest: false,
+      minWait: config.limit.window * 1000 + 1, // Set delay equal to just over the lifetime, * 1000 to convert to milliseconds
+      maxWait: config.limit.window * 1000 + 1, // Set delay equal to just over the lifetime, * 1000 to convert to milliseconds
+      lifetime: config.limit.window
+    });
+
+    return ratelimiter.prevent;
+}
