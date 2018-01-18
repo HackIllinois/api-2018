df274fa04ab8d203bbb1564d55db3f057c87d0ae
Arnav Sankaran <arnavsankaran@gmail.com>
2018-01-18 16:52:09 -0600

    Added missing docs to ExtraInfo model

--- api/v1/models/AttendeeExtraInfo.js
+++ api/v1/models/AttendeeExtraInfo.js
@@ -8,11 +8,21 @@ const AttendeeExtraInfo = Model.extend({
   }
 });
 
+/**
+ * Finds an attendee's extra information by its relational attendee's id
+ * @param  {Number|String} id the ID of the attendee with the appropriate type
+ * @return {Promise<Model>} a Promise resolving to the resulting AttendeeExtraInfo or null
+ */
 AttendeeExtraInfo.findByAttendeeId = (attendeeId) => AttendeeExtraInfo.where({
   attendee_id: attendeeId
 })
   .fetch();
 
+/**
+ * Finds an attendee's extra information by its ID
+ * @param  {Number|String} id the ID of the model with the appropriate type
+ * @return {Promise<Model>}   a Promise resolving to the resulting model or null
+ */
 AttendeeExtraInfo.findById = (id) => AttendeeExtraInfo.where({
   id: id
 })
