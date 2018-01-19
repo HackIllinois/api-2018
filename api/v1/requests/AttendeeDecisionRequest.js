fa26b032d0896a8f2022a1e0ed632f6464e3e1be
Arnav Sankaran <arnavsankaran@gmail.com>
2018-01-18 20:57:09 -0600

    Removed ecosystem from decision request

--- api/v1/requests/AttendeeDecisionRequest.js
+++ api/v1/requests/AttendeeDecisionRequest.js
@@ -2,13 +2,11 @@ const Request = require('./Request');
 const Attendee = require('../models/Attendee');
 
 const bodyRequired = ['priority', 'wave', 'status'];
-const bodyAllowed = [ 'acceptedEcosystemId' ];
 const attendee = new Attendee();
 const bodyValidations = {
   'priority': ['required', 'integer', 'max:10'],
   'wave': attendee.validations.wave.concat([ 'required' ]),
-  'status': attendee.validations.status.concat([ 'required' ]),
-  'acceptedEcosystemId': [ 'integer' ]
+  'status': attendee.validations.status.concat([ 'required' ])
 };
 
 function AttendeeDecisionRequest(headers, body) {
@@ -16,7 +14,6 @@ function AttendeeDecisionRequest(headers, body) {
 
   this.bodyRequired = bodyRequired;
   this.bodyValidations = bodyValidations;
-  this.bodyAllowed = bodyAllowed;
 }
 
 AttendeeDecisionRequest.prototype = Object.create(Request.prototype);
