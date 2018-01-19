b20395b5b780fe0df6990f8f333cceec5f85c13d
Arnav Sankaran <arnavsankaran@gmail.com>
2018-01-18 22:16:36 -0600

    Moved all attendee related sql code into createAttendees migration

--- database/migration/V20161226_0927__createAttendees.sql
+++ database/migration/V20161226_0927__createAttendees.sql
@@ -116,6 +116,47 @@ CREATE TABLE `attendee_project_interests` (
 		ON UPDATE NO ACTION
 );
 
+ALTER TABLE `attendees`
+ADD COLUMN `has_lightning_interest` TINYINT(1) NULL DEFAULT 0 AFTER `is_private`;
+
+INSERT INTO `mailing_lists` (`name`) VALUES ('lightning_talks');
+
+ALTER TABLE `attendees`
+ADD COLUMN `priority` TINYINT(1) UNSIGNED NULL DEFAULT 0 AFTER `phone_number`,
+ADD COLUMN `wave` TINYINT(1) UNSIGNED NULL DEFAULT 0 AFTER `priority`,
+ADD COLUMN `reviewer` VARCHAR(255) NULL DEFAULT NULL AFTER `wave`,
+ADD COLUMN `review_time` TIMESTAMP NULL DEFAULT NULL AFTER `reviewer`,
+ADD COLUMN `accepted_ecosystem_id` INT(10) UNSIGNED NULL DEFAULT NULL AFTER `review_time`,
+ADD COLUMN `acceptance_type` ENUM('CREATE', 'CONTRIBUTE') NULL DEFAULT NULL AFTER `accepted_ecosystem_id`,
+ADD INDEX `fk_attendees_ecosystem_id_idx` (`accepted_ecosystem_id` ASC);
+ALTER TABLE `attendees`
+ADD CONSTRAINT `fk_attendees_ecosystem_id`
+  FOREIGN KEY (`accepted_ecosystem_id`)
+  REFERENCES `ecosystems` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+INSERT INTO `mailing_lists` (`name`) VALUES ('wave_1');
+INSERT INTO `mailing_lists` (`name`) VALUES ('wave_2');
+INSERT INTO `mailing_lists` (`name`) VALUES ('wave_3');
+INSERT INTO `mailing_lists` (`name`) VALUES ('wave_4');
+INSERT INTO `mailing_lists` (`name`) VALUES ('wave_5');
+INSERT INTO `mailing_lists` (`name`) VALUES ('rejected');
+
+CREATE TABLE `attendee_rsvps` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `attendee_id` INT UNSIGNED NOT NULL,
+    `is_attending` TINYINT(1) NOT NULL,
+    `type` ENUM('CREATE', 'CONTRIBUTE'),
+    PRIMARY KEY (`id`),
+    INDEX `fk_attendee_rsvps_attendee_id_idx` (`attendee_id` ASC),
+CONSTRAINT `fk_attendee_rsvps_attendee_id`
+FOREIGN KEY (`attendee_id`)
+REFERENCES `attendees` (`id`)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+);
+
 CREATE TABLE `attendee_extra_infos` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`attendee_id` INT UNSIGNED NOT NULL,
